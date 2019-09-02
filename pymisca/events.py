5cdef19e11fb2de1e0ec68b987143a09c823577c
shouldsee <shouldsee.gem@gmail.com>
2019-09-02 21:51:02 +0000

    events.LinkEvent()

---
+++ pymisca/events.py
@@ -0,0 +1,27 @@
+from pymisca.shell import  file__notEmpty, dir__makeIfNeed, file__safeInode
+import os
+class CopyEvent(object):
+    def __init__(self, src,dest,force=0,delay=0):
+        return 
+    
+class LinkEvent(object):
+    def __repr__(self):
+        return "%r"%((self.src,self.dest,self.force),)
+    
+    def __init__(self,src,dest,force=0,delay=0):
+        self.src=src
+        self.dest=dest
+        self.force=force
+        
+        _f = file__safeInode
+        if _f(src)==_f(dest):
+            pass
+        else:
+            if file__notEmpty(dest):
+                if not force:
+                    return
+                else:
+                    os.remove(dest)
+            dir__makeIfNeed(fname=dest)
+            print dest
+            os.link(src,dest)
