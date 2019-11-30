c4b88b0a3a0b1e59d86c18fb44ede9271345d2f1
Travis CI <travis@travis-ci.org>
2019-11-30 23:57:17 +0000

    reindent

--- setup.py
+++ setup.py
@@ -15,25 +15,18 @@ import os,glob,sys
 # 'fisher@https://github.com/brentp/fishers_exact_test/archive/master.zip',]
 DIR= os.path.dirname(__file__)
 if DIR:
-	os.chdir(DIR)
+  os.chdir(DIR)
 FILE =  'requirements.txt'
 def parse_requirements():
     req = []
     dep = []
     for x in open(FILE,'r'):
-	x = x.strip()
-	if x.startswith("#"):
-	    pass
-        elif "@" in x:
-# 	    sp = x.split("@",1)
-# 	    req.append(sp[0])
-# 	    dep.append(sp[1])
-	    req.append(x)
-	elif " " in x:
-	    pass
-        else:
-	    req.append(x)
-    return req,dep
+      x = x.strip()
+      if x.startswith("#"):
+          pass
+      elif "@" in x:
+        req.append(x)
+      return req,dep
 
 required, dependency_links = parse_requirements()
 
@@ -48,9 +41,9 @@ required, dependency_links = parse_requirements()
 #required = ['git+https://github.com/shouldsee/mixem/archive/9ad994805009545f5befb65b8de9c877bb4f3137.zip']
 #print (required)
 setup(
-	name='pymisca',
-	version='0.1',
-	packages=['pymisca',
+  name='pymisca',
+  version='0.1',
+  packages=['pymisca',
               'pymisca.tensorflow_extra_',
               'pymisca.model_collection',
               'pymisca.iterative',
@@ -58,31 +51,19 @@ setup(
               'pymisca.lazydicts',
               'pymisca.atto_jobs_list',
              ],
-#   entry_points = {
-#           'console_scripts': [
-#               'command-name = pymisca.directory_hashmirror_0520:main',                  
-#           ],              
-#       },
     scripts = glob.glob('bin/*.py') ,
     package_data={'pymisca': ['*.sh','*.json','*.csv','*.tsv','*.npy','*.pk',
                               'templates/*.html',
-#                              'resources/*','genomeConfigs/*',
                              ],
-# #                  'runtime_data':['wraptool/*.{ext}'.format(**locals()) 
-# #                                  for ext in 
-# #                                  ['json','csv','tsv','npy','pk']],
                  },
     include_package_data=True,    
-#     scripts = {'bin':'*.py'},
-	license='GPL3',
-	author='Feng Geng',
-	author_email='shouldsee.gem@gmail.com',
-	long_description=open('README.md').read(),
-#	install_requires,
-	install_requires = required,
-	dependency_links = dependency_links,
-#		['numpy',
-#		'scipy',
-#		'matplotlib',]
+  license='GPL3',
+  author='Feng Geng',
+  author_email='shouldsee.gem@gmail.com',
+  long_description=open('README.md').read(),
+  install_requires = required,
+  dependency_links = dependency_links,
+#   ['numpy',
+#   'scipy',
+#   'matplotlib',]
 )
-
